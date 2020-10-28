9c2619c93e25052917bf78df85874297abfbf6ae
张成 <zc@yunshan.cn>
2020-10-28 11:10:53 +0800

    eureka部分

---
+++ springcloud-eureka/src/main/java/com/zack/springcloud/EurekaServer.java
@@ -0,0 +1,20 @@
+package com.zack.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * com.zack.springcloud
+ *
+ * @author zhangc
+ * @version 1.0
+ * @create 2020/10/28 11:05
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
